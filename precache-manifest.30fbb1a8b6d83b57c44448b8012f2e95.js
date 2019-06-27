60dcd6869be420248edc8fd46789158d25199386
Volodymyr Kryvytskyy <volodymyrkryvytskyy@MacBook-Pro.mate.academy>
2019-06-27 14:19:50 +0300

    Updates

---
+++ precache-manifest.30fbb1a8b6d83b57c44448b8012f2e95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a1273ab1cd041078e1eb83359fe3f3",
+    "url": "/github.io/dynamic-todo-list-react/index.html"
+  },
+  {
+    "revision": "c7fa9e55fb7047517cb1",
+    "url": "/github.io/dynamic-todo-list-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/github.io/dynamic-todo-list-react/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c7fa9e55fb7047517cb1",
+    "url": "/github.io/dynamic-todo-list-react/static/js/main.a43e0d1b.chunk.js"
+  },
+  {
+    "revision": "c51f7e68046fb4bbaa2a",
+    "url": "/github.io/dynamic-todo-list-react/static/js/runtime~main.658d4ebe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/dynamic-todo-list-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
